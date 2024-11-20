f51b37365b6926ce58040717aa447b565ca208a6
Fazilat Jahan <fazilat.jahan07@gmail.com>
2024-11-20 22:07:31 +0500

    Product Card Component Assignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,21 +7,21 @@ export default function Home() {
    
      
         <div className="place-items-center text-center gap-6 p-4 w-3/4 border border-cyan-700">
-          <Image src="/cricket kit 2.jfif" alt="cricket kit " width={150} height={150} className=" shadow-2xl rounded-2xl" />
+          <Image src="/cricket kit 3.jpg" alt="cricket kit " width={150} height={150} className=" shadow-2xl rounded-2xl" />
           <h1 className="font-bold m-2">Hard Ball Cricket Kit</h1>
           <h1 className="font-bold m-2">Price: 24,000/rs</h1>
           <button className="font-bold bg-teal-500 p-2 rounded-lg">Add To Cart</button>
         </div>
 
         <div className="place-items-center text-center gap-6 p-4 w-3/4 border border-cyan-700">
-          <Image src="/cricket kit 2.jfif" alt="cricket kit " width={150} height={150} className=" shadow-2xl rounded-2xl" />
+          <Image src="/cricket kit 3.jpg" alt="cricket kit " width={150} height={150} className=" shadow-2xl rounded-2xl" />
           <h1 className="font-bold m-2">Hard Ball Cricket Kit</h1>
           <h1 className="font-bold m-2">Price: 24,000/rs</h1>
           <button className="font-bold bg-teal-500 p-2 rounded-lg">Add To Cart</button>
         </div>
 
         <div className="place-items-center text-center gap-6  p-4 w-3/4 border border-cyan-700 ">
-          <Image src="/cricket kit 2.jfif" alt="cricket kit " width={150} height={150} className=" shadow-2xl rounded-2xl" />
+          <Image src="/cricket kit 3.jpg" alt="cricket kit " width={150} height={150} className=" shadow-2xl rounded-2xl" />
           <h1 className="font-bold m-2">Hard Ball Cricket Kit</h1>
           <h1 className="font-bold m-2">Price: 24,000/rs</h1>
           <button className="font-bold bg-teal-500 p-2 rounded-lg">Add To Cart</button>
